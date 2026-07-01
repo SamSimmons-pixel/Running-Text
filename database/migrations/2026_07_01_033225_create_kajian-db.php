c7583d90e3bcaf8e36e059adce69bf683d3c5830
SamSimmons-pixel <smro1432@gmail.com>
2026-07-01 11:16:29 +0700

    first commit

---
+++ database/migrations/2026_07_01_033225_create_kajian-db.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kajian', function (Blueprint $table) {
+            $table->id();
+            $table->DATETIME('Tanggal');
+            $table->string('Judul');
+            $table->string('Narasumber');
+            $table->string('Tempat');
+            $table->string('Kontak');
+            $table->boolean('Tampilkan')->default(false);
+            $table->string('Logo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
