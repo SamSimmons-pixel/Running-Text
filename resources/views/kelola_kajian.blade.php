773429a930645e1c12d03d51babadd8ae0df483f
SamSimmons-pixel <smro1432@gmail.com>
2026-07-06 16:00:26 +0700

    Moving Top Navbar to 1 asset

--- resources/views/kelola_kajian.blade.php
+++ resources/views/kelola_kajian.blade.php
@@ -13,63 +13,7 @@
   <body class="framed main-scrollable">
     <div class="wrapper">
 
-      {{-- ── Top Navbar ── --}}
-      <nav class="navbar navbar-static-top header-navbar">
-        <div class="header-navbar-mobile">
-          <div class="header-navbar-mobile__menu">
-            <button class="btn" type="button"><i class="fa fa-bars"></i></button>
-          </div>
-          <div class="header-navbar-mobile__title"><span>Kelola Kajian</span></div>
-          <div class="header-navbar-mobile__settings dropdown">
-            <a class="btn dropdown-toggle" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-power-off"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-right">
-              <li>
-                <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form-mobile').submit();">
-                  <i class="fa fa-sign-out"></i> Logout
-                </a>
-                <form id="logout-form-mobile" method="POST" action="{{ route('logout') }}" style="display:none;">@csrf</form>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="navbar-header">
-          <a class="navbar-brand" href="{{ route('admin.dashboard') }}">
-            <div class="logo text-nowrap">
-              <div class="logo__img"><i class="fa fa-book"></i></div>
-              <span class="logo__text">Jadwal Kajian</span>
-            </div>
-          </a>
-        </div>
-
-        <div class="topnavbar">
-          <ul class="nav navbar-nav navbar-left">
-            <li><a href="{{ route('admin.dashboard') }}"><span>Dashboard</span></a></li>
-            <li>
-              <a href="{{ url('/') }}" target="_blank">
-                <span><i class="fa fa-external-link" style="font-size:0.75em;"></i> MainPage</span>
-              </a>
-            </li>
-          </ul>
-          <ul class="userbar nav navbar-nav">
-            <li class="dropdown">
-              <a class="userbar__settings dropdown-toggle" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-power-off"></i>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-right">
-                <li>
-                  <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form-top').submit();">
-                    <i class="fa fa-sign-out"></i> Logout
-                  </a>
-                  <form id="logout-form-top" method="POST" action="{{ route('logout') }}" style="display:none;">@csrf</form>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      @include('partials.navbar')
 
       {{-- ── Dashboard Wrapper ── --}}
       <div class="dashboard">
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,62 @@
+{{-- ── Top Navbar ── --}}
+<nav class="navbar navbar-static-top header-navbar">
+  {{-- Mobile header --}}
+  <div class="header-navbar-mobile">
+    <div class="header-navbar-mobile__menu">
+      <button class="btn" type="button"><i class="fa fa-bars"></i></button>
+    </div>
+    <div class="header-navbar-mobile__title"><span>Admin Panel</span></div>
+    <div class="header-navbar-mobile__settings dropdown">
+      <a class="btn dropdown-toggle" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-power-off"></i>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-right">
+        <li>
+          <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form-mobile').submit();">
+            <i class="fa fa-sign-out"></i> Logout
+          </a>
+          <form id="logout-form-mobile" method="POST" action="{{ route('logout') }}" style="display:none;">@csrf</form>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  {{-- Brand --}}
+  <div class="navbar-header">
+    <a class="navbar-brand" href="{{ route('admin.dashboard') }}" wire:navigate>
+      <div class="logo text-nowrap">
+        <div class="logo__img"><i class="fa fa-book"></i></div>
+        <span class="logo__text">Admin Panel</span>
+      </div>
+    </a>
+  </div>
+
+  {{-- Desktop nav --}}
+  <div class="topnavbar">
+    <ul class="nav navbar-nav navbar-left">
+      <li class="{{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+        <a href="{{ route('admin.dashboard') }}" wire:navigate><span>Dashboard</span></a>
+      </li>
+      <li>
+        <a href="{{ url('/mainpage') }}" target="_blank">
+          <span><i class="fa fa-external-link" style="font-size:0.75em;"></i> MainPage</span>
+        </a>
+      </li>
+    </ul>
+    <ul class="userbar nav navbar-nav">
+      <li class="dropdown">
+        <a class="userbar__settings dropdown-toggle" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-power-off"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-right">
+          <li>
+            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form-top').submit();">
+              <i class="fa fa-sign-out"></i> Logout
+            </a>
+            <form id="logout-form-top" method="POST" action="{{ route('logout') }}" style="display:none;">@csrf</form>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
